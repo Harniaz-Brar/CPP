9bed783102d429c7850070260c8c926dcc2bd144
Harniaz Brar <harniazbrar@gmail.com>
2024-01-07 12:31:38 -0500

    Add files via upload

---
+++ FunctionsIntro/main.cpp
@@ -0,0 +1,28 @@
+/*
+// writing a program to find if number is even or odd (a,b)
+
+#include <iostream>
+using namespace std;
+
+// even = 1
+// odd = 0
+bool isEven (int a){
+    int (a&1);{
+        return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+int num;
+cin >> num;
+if (isEven(num))
+    {
+    cout << "number is even" << endl;
+    }
+else
+    {
+    cout << "Number is odd" << endl;
+    }
+}*/
