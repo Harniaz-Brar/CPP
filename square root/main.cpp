faeb91155b9610cad9002388131b6af7b546acb8
Harniaz Brar <harniazbrar@gmail.com>
2024-01-17 14:27:48 -0500

    Add files via upload

---
+++ square root/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    float n;
+    cout << "enter the number you want to find the sqare root of : " << " ";
+    cin >> n;
+    float j = pow(n,0.5);
+    cout << "square root is :" << " " << j;
+}
