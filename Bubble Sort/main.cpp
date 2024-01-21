c457d5c684af20ebbe6294e7aa4340f4b8c7ffcf
Harniaz Brar <harniazbrar@gmail.com>
2024-01-20 20:21:05 -0500

    Update main.cpp

--- Bubble Sort/main.cpp
+++ Bubble Sort/main.cpp
@@ -1,11 +1,19 @@
 void bubbleSort(vector<int>& arr, int n)
 {
+    bool isSwapped = false;
     for(int i = 0; i<n; i++) {
     // for round 1 to n-1
         for(int j=0; j<n-i; j++) {
             // process elements till n-i index
-            if(arr[j] > arr[j+1])
+            if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+            swapped = true;
+            }
+        }
+        if(swapped==false){
+            //already sorted
+            break;
         }
     }
+    
 }
