dc2d37259f60b2ab6c491115ea4581535847a72a
Harniaz Brar <harniazbrar@gmail.com>
2024-01-21 20:18:21 -0500

    Add files via upload

---
+++ STL Array/main.cpp
@@ -0,0 +1,24 @@
+// ARRAY
+
+#include<iostream>
+#include<array>
+
+using namespace std;
+int main() {
+    int basic[3] = {1,2,3};
+
+    array<int,4> a = {1,2,3,4};
+
+    int size = a.size();
+
+    for(int i=0; i<size; i++) {
+        cout << a[i] << endl;
+
+    }
+    cout<<"element at second index : " << a.at(2) << endl;
+
+    cout << "Empty or not : " << a.empty() << endl;
+
+    cout << "First Element : " <<a.front() << endl;
+    cout << "Last Element : " << a.back() << endl;
+}
